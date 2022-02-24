94f4b21086b906f44f693b757c996f168a9ff6ef
Santhosh Malay <santhosh_malay@epam.com>
2022-02-24 14:40:57 +0530

    using version2 of emoji

--- solution1.go
+++ solution1.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/kyokomi/emoji"
+	"github.com/kyokomi/emoji/v2"
 )
 
 func main() {
-	message := "\"Hello" + emoji.Sprint(" :world_map: !\"")
+	message := "\"Hello" + emoji.Sprint(" :world_map:!\"")
 	fmt.Println(message)
 }
