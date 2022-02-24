b40953acfe25ce49b501ed64f22a27d8adbcfbeb
Santhosh Malay <santhosh_malay@epam.com>
2022-02-24 13:29:17 +0530

    Removing the Intro directory

--- Intro_00/go.mod
+++
@@ -1,5 +0,0 @@
-module golang-united-school-homework-1/Intro_00
-
-go 1.17
-
-require github.com/kyokomi/emoji v2.2.4+incompatible
--- Intro_00/solution1.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/kyokomi/emoji"
-)
-
-func main() {
-	emoji.Println("\"Hello" + " :world_map:!\"")
-}
